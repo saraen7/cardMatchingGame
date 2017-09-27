75f88a9022c8671fc9210e8d647fa280982cf0f1
saraen7 <sara.e.norlin@hotmail.com>
2017-09-26 23:51:32 -0400

    Up through 2nd card plus some refactoring
    Clicking through the first two cards works. The code is much cleaner now.
    
    I still need to figure out why the alert is showing before the src changes even though the src is told to change first.
    
    Scoring will need to be figured out.
    
    "Reset game" is not working.

--- cardmatchinggame/js/main.js
+++ cardmatchinggame/js/main.js
@@ -1,73 +1,56 @@
-//clicking on "start game" shuffles the cards
+//clicking on "start game" shuffles the cards - DONE 9/20
 //clicking on "reset game" flips the cards to show the back of each card
-//clicking on a card shows its face
-//clicking on a second card that matches the first card, keeps both cards face-up
+//clicking on a card shows its face - DONE 9/20
+//clicking on a second card that matches the first card, keeps both cards face-up - DONE 9/20
 //clicking on a second card that does not match the face of the first card, flips both cards face down
 //clicking on a matching set, followed by a third card alerts the user
 //that alert contains the number of tries that the user attempted
 //last step - add a cheat mode that after one card is face up, on mouse over, expands the size of the matching face-down card
 
 $("document").ready(function(){ //loads the javascript after the HTML is mostly finished loading
+  $("button:first-of-type").on("click",function(){
 
-var cards = ["king","king","queen","queen"];
+    var cards = ["king","king","queen","queen"];
 
-//this function shuffles the array - https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-function shuffle(a) {
-    for (let i = a.length; i; i--) {
+    //this function shuffles the array - https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+    function shuffle(a) {
+      for (let i = a.length; i; i--) {
         let j = Math.floor(Math.random() * i);
         [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
     }
-}
 
-$("button:first-of-type").on("click",function(){ //clicking on the "start game" button
-  shuffle(cards); //this calls the shuffle function
-  $(".card1").addClass(cards[0]); //appends either king or queen class to the first card
-  $(".card2").addClass(cards[1]); //appends either king or queen class to the second card
-  $(".card3").addClass(cards[2]); //appends either king or queen class to the third card
-  $(".card4").addClass(cards[3]); //appends either king or queen class to the fourth card
-});
+    shuffle(cards); //this calls the shuffle function
+    $(".card1").addClass(cards[0]); //appends either king or queen class to the first card
+    $(".card2").addClass(cards[1]); //appends either king or queen class to the second card
+    $(".card3").addClass(cards[2]); //appends either king or queen class to the third card
+    $(".card4").addClass(cards[3]); //appends either king or queen class to the fourth card
 
+    if($(".faceUp").length==2 && $(".king").length==1){
+      alert("two cards selected, one card is a king");
+    }
 
-$(".card1").on("click",function(){
-  if($(".faceUp").length <= 1){ //if fewer than two cards are face up, turn card1 face up
-    $(".card1").addClass("faceUp"); //adds class "faceUp"
-    if($(".card1").hasClass("king")){
-      $(".card1").attr("src","images/King.png");//change the src tag to King if the class is King
-    } else {
-      $(".card1").attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
-  }
-}
-});
+    $("img").on("click",function(){
+      if($(".faceUp").length < 2){ //if less than 2 cards are face up, turn this card face up
+        $(this).addClass("faceUp"); //adds class "faceUp"
+        if($(this).hasClass("king")){
+          $(this).attr("src","images/King.png");//change the src tag to King if the class is King
+        } else {
+          $(this).attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
+        }
+      } else if (($(".faceUp.king").length == 2) || $(".faceUp.queen").length == 2) { //if two Kings are face up or two Queens are face up, turn the third card face up on click
+        $(this).addClass("faceUp"); //adds class "faceUp"
+        if($(this).hasClass("king")){
+          $(this).attr("src","images/King.png");//change the src tag to King if the class is King
+        } else {
+          $(this).attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
+        }
+        alert("You win! You took _ turns."); //I don't know why this alert fires before the src changes in the image.
 
-$(".card2").on("click",function(){
-  $(".card2").addClass("faceUp"); //adds class "faceUp"
-  if($(".card2").hasClass("king")){
-  $(".card2").attr("src","images/King.png");//change the src tag to King if the class is King
-} else {
-  $(".card2").attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
-}
-});
+      } else {
+        alert("face up length is greater than 2")
+      }
+    });
 
-$(".card3").on("click",function(){
-  $(".card3").addClass("faceUp"); //adds class "faceUp"
-  if($(".card3").hasClass("king")){
-  $(".card3").attr("src","images/King.png");//change the src tag to King if the class is King
-} else {
-  $(".card3").attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
-}
-});
-
-$(".card4").on("click",function(){
-  $(".card4").addClass("faceUp"); //adds class "faceUp"
-  if($(".card4").hasClass("king")){
-  $(".card4").attr("src","images/King.png");//change the src tag to King if the class is King
-} else {
-  $(".card4").attr("src","images/Queen.png");//change the src tag to Queen if the class is Queen
-}
-});//end card4 onclick
-//} //if 0 or 1 cards are faceUp
-
-//}); //end img on click
-
-
+  }); //end Start of Game
 }); //end loading javascript for safety
